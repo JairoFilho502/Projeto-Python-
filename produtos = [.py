a4c696b495b8ed4af01e4254390e520ab3fa1108
JairoFilho502 <jairogeorge.adv@gmail.com>
2025-06-02 12:00:14 -0300

    ajustes

--- produtos = [.py
+++ produtos = [.py
@@ -31,7 +31,7 @@ def exibir_lista(lista, titulo):
 
 def adicionar_ao_carrinho(produtos, carrinho):
     if len(carrinho) >= MAX_CARRINHO:
-        print("\nlimite máximo para o carrinho atingido(3 max), para continuar remova algum item.\n")
+        print("\nlimite máximo para o carrinho atingido(3 max), para continuar remova algum item.\n" + "-" * 85)
         return carrinho
     exibir_lista(produtos, "Veículos Disponíveis")
     escolha = input("Escolha o número do veículo: ")
